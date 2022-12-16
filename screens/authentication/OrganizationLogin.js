6ad6c53d1607c7bb5016d018ac26ce4cd3d03e5f
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2022-12-16 11:20:32 +0500

    Creating Organization Login & Registration

--- screens/authentication/OrganizationLogin.js
+++ screens/authentication/OrganizationLogin.js
@@ -1,10 +1,60 @@
-import { View, Text } from 'react-native'
+import { View, SafeAreaView, TouchableOpacity, StyleSheet, KeyboardAvoidingView} from 'react-native'
 import React from 'react'
+import { Provider as PaperProvider, Avatar, Text, TextInput, Button } from 'react-native-paper'
+import { useNavigation } from '@react-navigation/native';
 
-export default function OrganizationLogin() {
+export default function OrganizatonLogin() {
+    const navigation=useNavigation();
   return (
-    <View>
-      <Text>organizationLogin</Text>
-    </View>
+    <PaperProvider>
+        <SafeAreaView style={styles.container}>
+          <View style={styles.mainContainer}>
+            <Text style={styles.heading} variant="displayMedium">Login</Text>
+            <TextInput style={styles.usernameInput} mode={'outlined'} label={'Username'} ></TextInput>      
+            <TextInput style={styles.passwordInput} secureTextEntry mode={'outlined'} label={'Password'} ></TextInput>      
+            <TouchableOpacity onPress={()=>navigation.navigate('DoneeRegistration')}>
+              <Text style={styles.newUserBtn} variant="labelLarge">New User? Register Here!</Text>
+            </TouchableOpacity>
+            <Button style={styles.loginBtn} mode='contained' onPress={()=>navigation.navigate('OrganizationPortal')}>Login</Button>
+          </View>
+        </SafeAreaView>
+    </PaperProvider>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  mainContainer:{
+    flex:1,
+  },
+  heading:{
+    textAlign:'center',
+    marginTop:'15%',
+    marginBottom:'15%'
+  },
+  usernameInput:{
+    width:'75%',
+    marginLeft:'12%',
+    height:40
+  },
+  passwordInput:{
+    width:'75%',
+    marginLeft:'12%',
+    height:40,
+  },
+  newUserBtn:{
+    width:181,
+    marginLeft:'40%',
+    height:40,
+    color:'blue'
+
+  },
+  loginBtn:{
+    width:'75%',
+    marginLeft:'12%',
+    height:40,
+  }
+});
