cf2d47320e42cd4c2e86b49774341a3c568eed6d
Afaq Abid <afaqabid@gmail.com>
2023-01-04 12:03:13 +0500

    Organization Login Authentication Added

--- screens/authentication/OrganizationLogin.js
+++ screens/authentication/OrganizationLogin.js
@@ -2,20 +2,36 @@ import { View, SafeAreaView, TouchableOpacity, StyleSheet, KeyboardAvoidingView}
 import React from 'react'
 import { Provider as PaperProvider, Avatar, Text, TextInput, Button } from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native';
+import { useState, useEffect } from 'react';
+import { auth } from '../../firebase';
 
 export default function OrganizatonLogin() {
-    const navigation=useNavigation();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation();
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(userCredentials => {
+        const user = userCredentials.user;
+        console.log('Logged in with:', user.email);
+        navigation.navigate("OrganizationPortal")
+      })
+      .catch(error => alert(error.message))
+    }
+    
   return (
     <PaperProvider>
         <SafeAreaView style={styles.container}>
           <View style={styles.mainContainer}>
             <Text style={styles.heading} variant="displayMedium">Login</Text>
-            <TextInput style={styles.usernameInput} mode={'outlined'} label={'Username'} ></TextInput>      
-            <TextInput style={styles.passwordInput} secureTextEntry mode={'outlined'} label={'Password'} ></TextInput>      
+            <TextInput style={styles.usernameInput} mode={'outlined'} label={'Username'} value={email} onChangeText={text=>setEmail(text)} ></TextInput>      
+            <TextInput style={styles.passwordInput} secureTextEntry mode={'outlined'} label={'Password'} value={password} onChangeText={text=>setPassword(text)}></TextInput>      
             <TouchableOpacity onPress={()=>navigation.navigate('OrganizationRegistration')}>
               <Text style={styles.newUserBtn} variant="labelLarge">New User? Register Here!</Text>
             </TouchableOpacity>
-            <Button style={styles.loginBtn} mode='contained' onPress={()=>navigation.navigate('OrganizationPortal')}>Login</Button>
+            <Button style={styles.loginBtn} mode='contained' onPress={handleLogin()}>Login</Button>
           </View>
         </SafeAreaView>
     </PaperProvider>
