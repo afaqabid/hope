bb8f1df981fea8de06dbb019df2cb17991863f5c
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-05 21:41:08 +0500

    Setting text in Blogs

--- screens/blog/BlogTab.js
+++ screens/blog/BlogTab.js
@@ -7,24 +7,24 @@ export default function BlogTab() {
     <>
     <ScrollView >
       <Card style={styles.container}>
-        <Card.Cover source={{ uri: 'https://media.geeksforgeeks.org/wp-content/uploads/20220217151648/download3-200x200.png' }} />
+        <Card.Cover source={{ uri: 'https://www.preventionweb.net/sites/default/files/styles/landscape_16_9/public/2022-09/Pakistan%20laborer%20carries%20vegetables%20flood.jpg?h=0f52b544&itok=Z-sVkbzV' }} />
           <Card.Content>
-            <Text variant="titleLarge">Flood In America</Text>
-            <Text variant="bodyMedium">Roughly 18,000 years ago an ancient lake in Siberia about 120 km </Text>
+            <Text style={styles.heading} variant="titleLarge">Flood </Text>
+            <Text variant="bodyMedium">From 14 June to October 2022, floods in Pakistan killed 1,739 people, and caused ₨ 3.2 trillion of damage and ₨ 3.3 trillion of economic losses. The immediate causes of the floods were heavier than usual monsoon rains and melting glaciers that followed a severe heat wave, both of which are linked to climate change. </Text>
         </Card.Content>
      </Card>
      <Card style={styles.container}>
-        <Card.Cover source={{ uri: 'https://media.geeksforgeeks.org/wp-content/uploads/20220217151648/download3-200x200.png' }} />
+        <Card.Cover source={{ uri: 'https://propakistani.pk/wp-content/uploads/2022/03/Heatwave.jpg' }} />
           <Card.Content>
-            <Text variant="titleLarge">Flood In America</Text>
-            <Text variant="bodyMedium">Roughly 18,000 years ago an ancient lake in Siberia about 120 km </Text>
+            <Text variant="titleLarge">Heat Wave</Text>
+            <Text variant="bodyMedium">From March-May 2022, Pakistan recorded some of the highest temperatures in the country in the last 60 years. The heat waves from March to mid-June are now followed by the onset of the monsoon in parts of the country with flash flood warnings and torrential rains causing loss of life and damage to property. </Text>
         </Card.Content>
      </Card>
      <Card style={styles.container}>
-        <Card.Cover source={{ uri: 'https://media.geeksforgeeks.org/wp-content/uploads/20220217151648/download3-200x200.png' }} />
+        <Card.Cover source={{ uri: 'https://floodlist.com/wp-content/uploads/2021/07/Khyber-Pakhtunkhwa-damage-house-File-Photo.jpg' }} />
           <Card.Content>
-            <Text variant="titleLarge">Flood In America</Text>
-            <Text variant="bodyMedium">Roughly 18,000 years ago an ancient lake in Siberia about 120 km </Text>
+            <Text variant="titleLarge">Land-Slide</Text>
+            <Text variant="bodyMedium">Disaster authorities in Pakistan report that heavy rain has caused damage to homes and triggered landslides in Khyber Pakhtunkhwa (KP) Province from 20 January 2022</Text>
         </Card.Content>
      </Card>
    </ScrollView>
@@ -37,5 +37,8 @@ const styles = StyleSheet.create({
   container :{
     alignContent:'center',
     margin: 20,
-   }
+   },
+  heading :{
+    fontWeight: 'bold',
+  }
 })
