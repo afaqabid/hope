542f826a203d0573ca4742ebc6cfdecf6bc5f77f
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-05 18:29:45 +0500

    creating scroll view for blog tab using cards

--- screens/blog/BlogTab.js
+++ screens/blog/BlogTab.js
@@ -1,16 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, ScrollView } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Provider as PaperProvider } from 'react-native-paper'
+import { Card, Provider as PaperProvider } from 'react-native-paper'
 
 export default function BlogTab() {
   return (
-    <PaperProvider>
-        <SafeAreaView>
-            <Text>BlogTab</Text>
-        </SafeAreaView>
-    </PaperProvider>
-  )
-}
+    <>
+    <ScrollView >
+      <Card style={styles.container}>
+        <Card.Cover source={{ uri: 'https://media.geeksforgeeks.org/wp-content/uploads/20220217151648/download3-200x200.png' }} />
+          <Card.Content>
+            <Text variant="titleLarge">Flood In America</Text>
+            <Text variant="bodyMedium">Roughly 18,000 years ago an ancient lake in Siberia about 120 km </Text>
+        </Card.Content>
+     </Card>
+   </ScrollView>
+   </>
+  );
+};
 
-const styles = StyleSheet.create({})
+//const deviceWidth = Math.round(Dimensions.get('window').width);
+const styles = StyleSheet.create({
+  container :{
+    alignContent:'center',
+    margin: 25,
+   }
+})
