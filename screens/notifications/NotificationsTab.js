843c72828a4fde8b9b7b174c6a0969bb533b2436
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-07 00:23:56 +0500

    Importing Notification HEad

--- screens/notifications/NotificationsTab.js
+++ screens/notifications/NotificationsTab.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Provider as PaperProvider } from 'react-native-paper'
+import NotificationHead from '../../components/NotificationHead'
 
 export default function NotificationsTab() {
   return (
