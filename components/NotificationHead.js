239c1ff2487869ec31b233b79a9461e88f94b50c
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-07 00:18:08 +0500

    Setting Notificationshead  and  fixed bugs

--- components/NotificationHead.js
+++ components/NotificationHead.js
@@ -4,22 +4,37 @@ import { Avatar, Divider, Text, Provider as PaperProvider } from 'react-native-p
 import { useNavigation } from '@react-navigation/native';
 
 class NotificationUser {
-    constructor(username, userImg) {
-    this.userImg = userImg;
-    this.username = username;
-    }
+        constructor(notificationTitle, notificationName) {
+        this.notificationTitle = notificationTitle;
+        this.notificationName = notificationName;
+        }
 }
 
 
 export default function NotificationHead() {
-
-    var usersList=[]
+    var notificationPostList=[]
     var i=0;
     var size=10;
     for(i=0; i<size; i++)
     {
-        var x = new NotificationUser('User'+(i+1), '../assets/user.png');
-        usersList.push(x);
+        var x = new notificationPost('NotificationTitle'+(i+1), 'NotificationName'+(i+1));
+        notificationPostList.push(x);
     }
-    
-}
+    return (
+        <PaperProvider>
+            {
+                notificationPostList.map(x=>
+                    <>
+                        <View style={styles.donationCard}>
+                            <View style={styles.leftCard}>
+                                <TouchableOpacity onPress={()=>{alert(x.donationTitle + " is Toggled!")}} >
+                                    <Text style={styles.donationTitle}>{x.notificationTitle}</Text>
+                                    <Text style={styles.donorName}>{x.notificationName}</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </>
+                )
+            }
+        </PaperProvider>
+    )}
