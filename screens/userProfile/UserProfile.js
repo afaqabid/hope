e563c37ed0f01d45bef9bd5894b44e66f9769b85
Afaq Abid <afaqabid@gmail.com>
2023-01-07 23:48:20 +0500

    Added User Profile to the Portal.

--- screens/userProfile/UserProfile.js
+++ screens/userProfile/UserProfile.js
@@ -1,14 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { SafeAreaView} from 'react-native-safe-area-context'
-import { TouchableRipple } from 'react-native-paper'
-
 
 export default function UserProfile() {
   return (
-    <SafeAreaView>
-      <Text>This is User Profile Screen.</Text>
-    </SafeAreaView>
+    <View>
+      <Text>UserProfile</Text>
+    </View>
   )
 }
 
