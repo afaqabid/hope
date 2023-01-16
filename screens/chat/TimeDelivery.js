9579c28e82bdc6d0e03ab4399a25656fadbd5078
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-16 16:24:32 +0500

    Applying check marks for read message

---
+++ screens/chat/TimeDelivery.js
@@ -0,0 +1,29 @@
+import moment from 'moment';
+import { Icon } from 'native-base';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const TimeDelivery = (props) => {
+    const { sender, item } = props;
+    return (
+        <View
+            style={[styles.mainView, {
+                justifyContent: 'flex-end',
+            }]}
+        >
+            <Text style={{
+                fontFamily: 'Poppins-Regular', 
+                fontSize: 7,
+                color: sender ? COLORS.white : COLORS.black
+            }}>
+                {moment(item.send_time).format('LLL')}
+            </Text>
+                <Icon
+                    name = {"checkmark-done"}
+                    type = "Ionicons"
+                    style={{color: item.seen ? COLORS.black : COLORS.white , fontSize: 15, marginLeft: 5}}
+                />
+
+        </View>
+    );
+};
