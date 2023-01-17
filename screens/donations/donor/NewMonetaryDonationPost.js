2dad048a6972dc3533956558519992ab8500c760
Afaq Abid <afaqabid@gmail.com>
2023-01-18 03:07:24 +0500

    Donation Types Added!

---
+++ screens/donations/donor/NewMonetaryDonationPost.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function NewMonetaryDonationPost() {
+  return (
+    <View>
+      <Text>NewMonetaryDonationPost</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
