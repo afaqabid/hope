beff7e3e8a7c256968c67e5230ba7f0e246678ee
Zainab-Ijaz <f190917@cfd.nu.edu.pk>
2023-01-19 18:56:57 +0500

    CollectedDonation tab for organization and fixed bugs

---
+++ screens/donations/organization/CollectedDonationTab.js
@@ -0,0 +1,29 @@
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Provider as PaperProvider, Button, Text } from 'react-native-paper'
+import { useFonts } from 'expo-font'
+import { useNavigation } from '@react-navigation/native'
+ 
+export default function CollectedDonationTab() {
+    let [fontLoaded]=useFonts({
+        'Manrope-Bold': require('../../../assets/fonts/Manrope-Bold.ttf'),
+        'Manrope-ExtraBold': require('../../../assets/fonts/Manrope-ExtraBold.ttf'),
+        'Manrope-ExtraLight': require('../../../assets/fonts/Manrope-ExtraLight.ttf'),
+        'Manrope-Light': require('../../../assets/fonts/Manrope-Light.ttf'),
+        'Manrope-Medium': require('../../../assets/fonts/Manrope-Medium.ttf'),
+        'Manrope-Regular': require('../../../assets/fonts/Manrope-Regular.ttf'),
+        'Manrope-SemiBold': require('../../../assets/fonts/Manrope-SemiBold.ttf'),
+    });
+
+    const navigation = useNavigation();
+
+    return (
+    <PaperProvider>
+        <text>Total Donation is 00000/rs</text>
+    </PaperProvider>
+  )
+}
+
+const styles = StyleSheet.create({
+})
