ad36384b733865ab57926fa8de0b1e19f0791eaf
Afaq Abid <afaqabid@gmail.com>
2023-03-27 22:21:07 +0500

    Redesigned Request Tab.

--- screens/requests/RequestsTab.js
+++ screens/requests/RequestsTab.js
@@ -1,20 +1,20 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Provider as PaperProvider } from 'react-native-paper'
-import RequestHead from '../../components/RequestHead'
-
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Provider as PaperProvider } from "react-native-paper";
+import RequestHead from "../../components/RequestHead";
+import Colors from "../../assets/constants/Colors";
 
 export default function RequestsTab() {
   return (
-    <PaperProvider>          
-          <View style={{backgroundColor:'#EFECEB'}}>
-            <ScrollView>
-              <RequestHead></RequestHead>
-            </ScrollView>
-          </View>
+    <PaperProvider>
+      <View style={{ backgroundColor: Colors.background }}>
+        <ScrollView>
+          <RequestHead></RequestHead>
+        </ScrollView>
+      </View>
     </PaperProvider>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
