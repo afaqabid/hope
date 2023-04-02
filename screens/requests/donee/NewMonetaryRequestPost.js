f80317650acd387e96982a7960d72b5a50556ea4
Afaq Abid <afaqabid@gmail.com>
2023-04-03 00:45:52 +0500

    Updated Monetary Request.

--- screens/requests/donee/NewMonetaryRequestPost.js
+++ screens/requests/donee/NewMonetaryRequestPost.js
@@ -1,190 +1,195 @@
-import {Keyboard,  KeyboardAvoidingView, ScrollView, StyleSheet, TouchableOpacity, View, TextInput, TouchableWithoutFeedback} from 'react-native'
-import React from 'react'
-import { Provider as PaperProvider, Text, Button} from 'react-native-paper'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useFonts } from 'expo-font'
-import { useNavigation } from '@react-navigation/native'
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React from "react";
+import { Provider as PaperProvider, Text, Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { useNavigation } from "@react-navigation/native";
 
 export default function NewMonetaryRequestPost() {
-  let [fontLoaded]=useFonts({
-        'Manrope-Bold': require('../../../assets/fonts/Manrope-Bold.ttf'),
-        'Manrope-ExtraBold': require('../../../assets/fonts/Manrope-ExtraBold.ttf'),
-        'Manrope-ExtraLight': require('../../../assets/fonts/Manrope-ExtraLight.ttf'),
-        'Manrope-Light': require('../../../assets/fonts/Manrope-Light.ttf'),
-        'Manrope-Medium': require('../../../assets/fonts/Manrope-Medium.ttf'),
-        'Manrope-Regular': require('../../../assets/fonts/Manrope-Regular.ttf'),
-        'Manrope-SemiBold': require('../../../assets/fonts/Manrope-SemiBold.ttf'),
-    });
+  let [fontLoaded] = useFonts({
+    "Manrope-Bold": require("../../../assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-ExtraBold": require("../../../assets/fonts/Manrope-ExtraBold.ttf"),
+    "Manrope-ExtraLight": require("../../../assets/fonts/Manrope-ExtraLight.ttf"),
+    "Manrope-Light": require("../../../assets/fonts/Manrope-Light.ttf"),
+    "Manrope-Medium": require("../../../assets/fonts/Manrope-Medium.ttf"),
+    "Manrope-Regular": require("../../../assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-SemiBold": require("../../../assets/fonts/Manrope-SemiBold.ttf"),
+  });
 
-    const HideKeyboard = ({ children }) => (
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        {children}
-      </TouchableWithoutFeedback>
-      );
+  const HideKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
 
-      const navigation = useNavigation();
+  const navigation = useNavigation();
 
-      const postRequest = () => {
-        if (title.trim() == "" || description.trim() == "" || amount < 0) {
-          alert("Please Insert All Fields!\nMinimum amount is 100 Rupees.");
-        } else {
-          alert("Posted Donation Request Successfully!");
-          navigation.navigate("DoneePortal");
-        }
-      };
-
-      return (
-        <PaperProvider>
-          <SafeAreaView style={styles.mainContainer}>
-            <KeyboardAwareScrollView>
-              <ScrollView>
-                <View style={styles.detailsContainer}>
-                  <Text style={styles.titleTxt}>Request Amount</Text>
-                  <TextInput
-                    style={styles.donationTitle}
-                    placeholder="Enter Amount"
-                    keyboardType="numeric"
-                    onChangeText={(text) => {
-                      setAmount(text);
-                    }}
-                  ></TextInput>
-                  <Text style={styles.titleTxt}>Request Title</Text>
-                  <TextInput
-                    style={styles.donationTitle}
-                    placeholder="Write Request Title Here."
-                    onChangeText={(text) => {
-                      setTitle(text);
-                    }}
-                  ></TextInput>
-                  <Text style={styles.titleTxt}>Description</Text>
-                  <TextInput
-                    multiline={true}
-                    style={styles.description}
-                    placeholder="Write Request Description Here."
-                    onChangeText={(text) => {
-                      setDescription(text);
-                    }}
-                  ></TextInput>
-                  <View style={styles.btnContainer}>
-                    <Button style={styles.btnPost} onPress={postRequest}>
-                      <Text style={styles.btnTxtPost}>Post</Text>
-                    </Button>
-                    <Button style={styles.btnCancel}>
-                      <Text
-                        style={styles.btnTxtCancel}
-                        onPress={() => {
-                          navigation.navigate("NewRequestPost");
-                        }}
-                      >
-                        Cancel
-                      </Text>
-                    </Button>
-                  </View>
-                </View>
-              </ScrollView>
-            </KeyboardAwareScrollView>
-          </SafeAreaView>
-        </PaperProvider>
-      );
+  const postRequest = () => {
+    if (title.trim() == "" || description.trim() == "" || amount < 0) {
+      alert("Please Insert All Fields!\nMinimum amount is 100 Rupees.");
+    } else {
+      alert("Posted Request Successfully!");
+      navigation.navigate("DoneePortal");
     }
+  };
+
+  return (
+    <PaperProvider>
+      <SafeAreaView style={styles.mainContainer}>
+        <KeyboardAwareScrollView>
+          <ScrollView>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.titleTxt}>Request Amount</Text>
+              <TextInput
+                style={styles.requestTitle}
+                placeholder="Enter Amount"
+                keyboardType="numeric"
+                onChangeText={(text) => {
+                  setAmount(text);
+                }}
+              ></TextInput>
+              <Text style={styles.titleTxt}>Request Title</Text>
+              <TextInput
+                style={styles.requestTitle}
+                placeholder="Write Request Title Here."
+                onChangeText={(text) => {
+                  setTitle(text);
+                }}
+              ></TextInput>
+              <Text style={styles.titleTxt}>Description</Text>
+              <TextInput
+                multiline={true}
+                style={styles.description}
+                placeholder="Write Request Description Here."
+                onChangeText={(text) => {
+                  setDescription(text);
+                }}
+              ></TextInput>
+              <View style={styles.btnContainer}>
+                <Button style={styles.btnPost} onPress={postRequest}>
+                  <Text style={styles.btnTxtPost}>Post</Text>
+                </Button>
+                <Button style={styles.btnCancel}>
+                  <Text
+                    style={styles.btnTxtCancel}
+                    onPress={() => {
+                      navigation.navigate("NewRequestPost");
+                    }}
+                  >
+                    Cancel
+                  </Text>
+                </Button>
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAwareScrollView>
+      </SafeAreaView>
+    </PaperProvider>
+  );
+}
 
 const styles = StyleSheet.create({
-  mainContainer:{
-    flex:1,
-    display:'flex',
-    alignItems:'center',
-    backgroundColor:'#FDFAF6',
+  mainContainer: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#FDFAF6",
   },
-  imageContainer:{
-    height:'80%',
-    backgroundColor:'white',
-    width:'100%',
-    margin:0,
-    borderRadius:10,
-    padding:10
-  },
-
-  detailsContainer:{
-    marginTop:'40%',
-    height:'60%',
-    backgroundColor:'transparent',
-    width:'100%',
-    margin:10,
-    borderRadius:10,
-    display:'flex',
+  imageContainer: {
+    height: "80%",
+    backgroundColor: "white",
+    width: "100%",
+    margin: 0,
+    borderRadius: 10,
+    padding: 10,
   },
 
-  titleTxt:{
-    fontSize:20,
-    color:'#1C702B',
-    fontFamily:'Manrope-ExtraBold',
-    marginLeft:5,
-    marginBottom:0
-
+  detailsContainer: {
+    marginTop: "40%",
+    height: "60%",
+    backgroundColor: "transparent",
+    width: "100%",
+    margin: 10,
+    borderRadius: 10,
+    display: "flex",
   },
 
-  donationTitle:{
-    height:45,
-    fontFamily:'Manrope-Regular',
-    width:'95%',
-    fontSize:18,
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:5,
-    
-  },
-  
-  description:{
-    height:45,
-    fontFamily:'Manrope-Regular',
-    width:'95%',
-    fontSize:18,
-    backgroundColor:'white',
-    padding:10,
-    height:200,
-    borderRadius:5,
+  titleTxt: {
+    fontSize: 20,
+    color: "#1C702B",
+    fontFamily: "Manrope-ExtraBold",
+    marginLeft: 5,
+    marginBottom: 0,
   },
 
-  btnContainer:{
-    display:'flex',
-    flexDirection:'row',
-    marginTop:5,
+  requestTitle: {
+    height: 45,
+    fontFamily: "Manrope-Regular",
+    width: "95%",
+    fontSize: 18,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
   },
-  btnPost:{
+
+  description: {
+    height: 45,
+    fontFamily: "Manrope-Regular",
+    width: "95%",
+    fontSize: 18,
+    backgroundColor: "white",
+    padding: 10,
+    height: 200,
+    borderRadius: 5,
+  },
+
+  btnContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  btnPost: {
     backgroundColor: "#1C702B",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    height:55,
-    minWidth:'45%',
-    marginTop:10,
+    height: 55,
+    minWidth: "45%",
+    marginTop: 10,
   },
-  btnCancel:{
+  btnCancel: {
     backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    height:55,
-    minWidth:'45%',
-    borderWidth:2,
-    borderColor:'#1C702B',
-    marginTop:10,
-    marginLeft:10,
-
+    height: 55,
+    minWidth: "45%",
+    borderWidth: 2,
+    borderColor: "#1C702B",
+    marginTop: 10,
+    marginLeft: 10,
   },
-  btnTxtPost:{
-    color:'white', 
-    fontSize:18, 
-    fontFamily:'Manrope-Bold', 
-    justifyContent:'center',
-    alignItems:'center',
+  btnTxtPost: {
+    color: "white",
+    fontSize: 18,
+    fontFamily: "Manrope-Bold",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  btnTxtCancel:{
+  btnTxtCancel: {
     color: "#1C702B",
-    fontSize:18,
-    fontFamily:'Manrope-Bold', 
-    justifyContent:'center',
-    alignItems:'center'
-  }
-  
-})
+    fontSize: 18,
+    fontFamily: "Manrope-Bold",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
