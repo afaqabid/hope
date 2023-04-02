367b503f7f09cce7b885c27fe73e77c0cc8d7094
Afaq Abid <afaqabid@gmail.com>
2023-04-02 22:14:51 +0500

    Added Validation Checks.

--- screens/donations/donor/NewMonetaryDonationPost.js
+++ screens/donations/donor/NewMonetaryDonationPost.js
@@ -1,157 +1,200 @@
-import {Keyboard,  KeyboardAvoidingView, ScrollView, StyleSheet, TouchableOpacity, View, TextInput, TouchableWithoutFeedback} from 'react-native'
-import React from 'react'
-import { Provider as PaperProvider, Text, Button} from 'react-native-paper'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useFonts } from 'expo-font'
-import { useNavigation } from '@react-navigation/native'
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React, { useState } from "react";
+import { Provider as PaperProvider, Text, Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { useNavigation } from "@react-navigation/native";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 export default function NewMonetaryDonationPost() {
-  let [fontLoaded]=useFonts({
-        'Manrope-Bold': require('../../../assets/fonts/Manrope-Bold.ttf'),
-        'Manrope-ExtraBold': require('../../../assets/fonts/Manrope-ExtraBold.ttf'),
-        'Manrope-ExtraLight': require('../../../assets/fonts/Manrope-ExtraLight.ttf'),
-        'Manrope-Light': require('../../../assets/fonts/Manrope-Light.ttf'),
-        'Manrope-Medium': require('../../../assets/fonts/Manrope-Medium.ttf'),
-        'Manrope-Regular': require('../../../assets/fonts/Manrope-Regular.ttf'),
-        'Manrope-SemiBold': require('../../../assets/fonts/Manrope-SemiBold.ttf'),
-    });
+  let [fontLoaded] = useFonts({
+    "Manrope-Bold": require("../../../assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-ExtraBold": require("../../../assets/fonts/Manrope-ExtraBold.ttf"),
+    "Manrope-ExtraLight": require("../../../assets/fonts/Manrope-ExtraLight.ttf"),
+    "Manrope-Light": require("../../../assets/fonts/Manrope-Light.ttf"),
+    "Manrope-Medium": require("../../../assets/fonts/Manrope-Medium.ttf"),
+    "Manrope-Regular": require("../../../assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-SemiBold": require("../../../assets/fonts/Manrope-SemiBold.ttf"),
+  });
 
-    const HideKeyboard = ({ children }) => (
-      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        {children}
-      </TouchableWithoutFeedback>
-      );
+  const [amount, setAmount] = useState(0);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-      const navigation = useNavigation();
+  const HideKeyboard = ({ children }) => (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      {children}
+    </TouchableWithoutFeedback>
+  );
+
+  const navigation = useNavigation();
+
+  const postDonation = () => {
+    if (title.trim() == "" || description.trim() == "" || amount < 0) {
+      alert("Please Insert All Fields!\nMinimum amount is 100 Rupees.");
+    } else {
+      alert("Posted Donation Request Successfully!");
+      navigation.navigate("DonorPortal");
+    }
+  };
 
   return (
     <PaperProvider>
-      <HideKeyboard>
       <SafeAreaView style={styles.mainContainer}>
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "position" : "height"} style={{flex:1}} >
-        <ScrollView>
-        <View style={styles.detailsContainer}>
-          <Text style={styles.titleTxt}>Donation Amount</Text>
-          <TextInput style={styles.donationTitle} placeholder="Enter Amount" ></TextInput>
-          <Text style={styles.titleTxt}>Donation Title</Text>
-          <TextInput style={styles.donationTitle} placeholder="Write Donation Title Here." ></TextInput>
-          <Text style={styles.titleTxt}>Description</Text>
-          <TextInput multiline={true} style={styles.description} placeholder="Write Donation Description Here."></TextInput>
-          <View style={styles.btnContainer}>
-            <Button style={styles.btnPost}>
-              <Text style={styles.btnTxtPost}>Post</Text>
-            </Button>
-            <Button style={styles.btnCancel}>
-              <Text style={styles.btnTxtCancel} onPress={()=>{navigation.navigate('NewDonationPost')}}>Cancel</Text>
-            </Button>
-          </View>
-        </View>
-        </ScrollView>
-      </KeyboardAvoidingView>
+        <KeyboardAwareScrollView>
+          <ScrollView>
+            <View style={styles.detailsContainer}>
+              <Text style={styles.titleTxt}>Donation Amount</Text>
+              <TextInput
+                style={styles.donationTitle}
+                placeholder="Enter Amount"
+                keyboardType="numeric"
+                onChangeText={(text) => {
+                  setAmount(text);
+                }}
+              ></TextInput>
+              <Text style={styles.titleTxt}>Donation Title</Text>
+              <TextInput
+                style={styles.donationTitle}
+                placeholder="Write Donation Title Here."
+                onChangeText={(text) => {
+                  setTitle(text);
+                }}
+              ></TextInput>
+              <Text style={styles.titleTxt}>Description</Text>
+              <TextInput
+                multiline={true}
+                style={styles.description}
+                placeholder="Write Donation Description Here."
+                onChangeText={(text) => {
+                  setDescription(text);
+                }}
+              ></TextInput>
+              <View style={styles.btnContainer}>
+                <Button style={styles.btnPost} onPress={postDonation}>
+                  <Text style={styles.btnTxtPost}>Post</Text>
+                </Button>
+                <Button style={styles.btnCancel}>
+                  <Text
+                    style={styles.btnTxtCancel}
+                    onPress={() => {
+                      navigation.navigate("NewDonationPost");
+                    }}
+                  >
+                    Cancel
+                  </Text>
+                </Button>
+              </View>
+            </View>
+          </ScrollView>
+        </KeyboardAwareScrollView>
       </SafeAreaView>
-    </HideKeyboard>
     </PaperProvider>
-    
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  mainContainer:{
-    flex:1,
-    display:'flex',
-    alignItems:'center',
-    backgroundColor:'#FDFAF6',
+  mainContainer: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#FDFAF6",
   },
-  imageContainer:{
-    height:'80%',
-    backgroundColor:'white',
-    width:'100%',
-    margin:0,
-    borderRadius:10,
-    padding:10
-  },
-
-  detailsContainer:{
-    marginTop:'40%',
-    height:'60%',
-    backgroundColor:'transparent',
-    width:'100%',
-    margin:10,
-    borderRadius:10,
-    display:'flex',
+  imageContainer: {
+    height: "80%",
+    backgroundColor: "white",
+    width: "100%",
+    margin: 0,
+    borderRadius: 10,
+    padding: 10,
   },
 
-  titleTxt:{
-    fontSize:20,
-    color:'#1C702B',
-    fontFamily:'Manrope-ExtraBold',
-    marginLeft:5,
-    marginBottom:0
-
+  detailsContainer: {
+    marginTop: "40%",
+    height: "60%",
+    backgroundColor: "transparent",
+    width: "100%",
+    margin: 10,
+    borderRadius: 10,
+    display: "flex",
   },
 
-  donationTitle:{
-    height:45,
-    fontFamily:'Manrope-Regular',
-    width:'95%',
-    fontSize:18,
-    backgroundColor:'white',
-    padding:10,
-    borderRadius:5,
-    
-  },
-  
-  description:{
-    height:45,
-    fontFamily:'Manrope-Regular',
-    width:'95%',
-    fontSize:18,
-    backgroundColor:'white',
-    padding:10,
-    height:200,
-    borderRadius:5,
+  titleTxt: {
+    fontSize: 20,
+    color: "#1C702B",
+    fontFamily: "Manrope-ExtraBold",
+    marginLeft: 5,
+    marginBottom: 0,
   },
 
-  btnContainer:{
-    display:'flex',
-    flexDirection:'row',
-    marginTop:5,
+  donationTitle: {
+    height: 45,
+    fontFamily: "Manrope-Regular",
+    width: "95%",
+    fontSize: 18,
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
   },
-  btnPost:{
+
+  description: {
+    height: 45,
+    fontFamily: "Manrope-Regular",
+    width: "95%",
+    fontSize: 18,
+    backgroundColor: "white",
+    padding: 10,
+    height: 200,
+    borderRadius: 5,
+  },
+
+  btnContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+  },
+  btnPost: {
     backgroundColor: "#1C702B",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    height:55,
-    minWidth:'45%',
-    marginTop:10,
+    height: 55,
+    minWidth: "45%",
+    marginTop: 10,
   },
-  btnCancel:{
+  btnCancel: {
     backgroundColor: "white",
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 5,
-    height:55,
-    minWidth:'45%',
-    borderWidth:2,
-    borderColor:'#1C702B',
-    marginTop:10,
-    marginLeft:10,
-
+    height: 55,
+    minWidth: "45%",
+    borderWidth: 2,
+    borderColor: "#1C702B",
+    marginTop: 10,
+    marginLeft: 10,
   },
-  btnTxtPost:{
-    color:'white', 
-    fontSize:18, 
-    fontFamily:'Manrope-Bold', 
-    justifyContent:'center',
-    alignItems:'center',
+  btnTxtPost: {
+    color: "white",
+    fontSize: 18,
+    fontFamily: "Manrope-Bold",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  btnTxtCancel:{
+  btnTxtCancel: {
     color: "#1C702B",
-    fontSize:18,
-    fontFamily:'Manrope-Bold', 
-    justifyContent:'center',
-    alignItems:'center'
-  }
-  
-})
+    fontSize: 18,
+    fontFamily: "Manrope-Bold",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
