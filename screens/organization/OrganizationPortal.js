29f50c4ea31a1739334d70726b89e4d4d460e786
Afaq Abid <afaqabid@gmail.com>
2023-04-03 00:56:43 +0500

    Created Organization Profile Screen.

--- screens/organization/OrganizationPortal.js
+++ screens/organization/OrganizationPortal.js
@@ -1,35 +1,45 @@
-import * as React from 'react';
-import { BottomNavigation, Text, Provider as PaperProvider, Appbar, Surface } from 'react-native-paper';
-import { Alert, StyleSheet } from 'react-native';
-import CollectedDonationTab from '../donations/organization/CollectedDonationTab';
-import NotificationsTab from '../notifications/NotificationsTab';
-import { auth } from '../../firebase';
-import { useNavigation } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import Colors from '../../assets/constants/Colors';
+import * as React from "react";
+import {
+  BottomNavigation,
+  Text,
+  Provider as PaperProvider,
+  Appbar,
+  Surface,
+} from "react-native-paper";
+import { Alert, StyleSheet } from "react-native";
+import CollectedDonationTab from "../donations/organization/CollectedDonationTab";
+import NotificationsTab from "../notifications/NotificationsTab";
+import { auth } from "../../firebase";
+import { useNavigation } from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import Colors from "../../assets/constants/Colors";
 
-const TabOne = () => <CollectedDonationTab/>
-const TabTwo = () => <NotificationsTab/>
+const TabOne = () => <CollectedDonationTab />;
+const TabTwo = () => <NotificationsTab />;
 
-export default function OrganizationPortal (){
-  let [fontLoaded]=useFonts({
-    'Manrope-Bold': require('../../assets/fonts/Manrope-Bold.ttf'),
-    'Manrope-ExtraBold': require('../../assets/fonts/Manrope-ExtraBold.ttf'),
-    'Manrope-ExtraLight': require('../../assets/fonts/Manrope-ExtraLight.ttf'),
-    'Manrope-Light': require('../../assets/fonts/Manrope-Light.ttf'),
-    'Manrope-Medium': require('../../assets/fonts/Manrope-Medium.ttf'),
-    'Manrope-Regular': require('../../assets/fonts/Manrope-Regular.ttf'),
-    'Manrope-SemiBold': require('../../assets/fonts/Manrope-SemiBold.ttf'),
+export default function OrganizationPortal() {
+  let [fontLoaded] = useFonts({
+    "Manrope-Bold": require("../../assets/fonts/Manrope-Bold.ttf"),
+    "Manrope-ExtraBold": require("../../assets/fonts/Manrope-ExtraBold.ttf"),
+    "Manrope-ExtraLight": require("../../assets/fonts/Manrope-ExtraLight.ttf"),
+    "Manrope-Light": require("../../assets/fonts/Manrope-Light.ttf"),
+    "Manrope-Medium": require("../../assets/fonts/Manrope-Medium.ttf"),
+    "Manrope-Regular": require("../../assets/fonts/Manrope-Regular.ttf"),
+    "Manrope-SemiBold": require("../../assets/fonts/Manrope-SemiBold.ttf"),
   });
-  
 
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'tabOne', title: 'Collected Donations', focusedIcon: 'charity'},
-    { key: 'tabTwo', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline' },
+    { key: "tabOne", title: "Collected Donations", focusedIcon: "charity" },
+    {
+      key: "tabTwo",
+      title: "Notifications",
+      focusedIcon: "bell",
+      unfocusedIcon: "bell-outline",
+    },
   ]);
 
-  const appbarTitle = ['Donation', 'Notifications'];
+  const appbarTitle = ["Donation", "Notifications"];
 
   const renderScene = BottomNavigation.SceneMap({
     tabOne: TabOne,
@@ -39,72 +49,93 @@ export default function OrganizationPortal (){
   const navigation = useNavigation();
 
   const handleLogout = () => {
-    Alert.alert("Are You Sure?","You'll be logged out!",[
-      {text:"Yes", onPress: ()=>{
-        auth.signOut().then(() => {
-          console.log("User Signed Out Successfully!");
-          navigation.navigate("SelectPortal")
-          })
-          .catch(error => alert(error.message))
-      }},
-      {text:"No"},
-    ])
-  }
+    Alert.alert("Are You Sure?", "You'll be logged out!", [
+      {
+        text: "Yes",
+        onPress: () => {
+          auth
+            .signOut()
+            .then(() => {
+              console.log("User Signed Out Successfully!");
+              navigation.navigate("SelectPortal");
+            })
+            .catch((error) => alert(error.message));
+        },
+      },
+      { text: "No" },
+    ]);
+  };
   const handleProfile = () => {
-      navigation.navigate('UserProfile');
-  }
+    navigation.navigate("OrganizationUserProfile");
+  };
 
   return (
     <PaperProvider>
-        <Surface style={styles.surface} elevation={4}>
-      <Appbar.Header style={styles.topBar} >
-        <Appbar.Action iconColor='#1C702B' icon="account" size={40} onPress={handleProfile} />
-        <Text style={styles.appbarTitle}>{appbarTitle[index]}</Text>
-        <Appbar.Action iconColor='#1C702B' style={styles.appbarLogout} size={30} icon="logout" onPress={handleLogout} />
-      </Appbar.Header>
-  </Surface>
+      <Surface style={styles.surface} elevation={4}>
+        <Appbar.Header style={styles.topBar}>
+          <Appbar.Action
+            iconColor="#1C702B"
+            icon="account"
+            size={40}
+            onPress={handleProfile}
+          />
+          <Text style={styles.appbarTitle}>{appbarTitle[index]}</Text>
+          <Appbar.Action
+            iconColor="#1C702B"
+            style={styles.appbarLogout}
+            size={30}
+            icon="logout"
+            onPress={handleLogout}
+          />
+        </Appbar.Header>
+      </Surface>
       <BottomNavigation
         navigationState={{ index, routes }}
         onIndexChange={setIndex}
         renderScene={renderScene}
-        barStyle={{ backgroundColor: Colors.background, width:'100%', elevation:'none', color:Colors.background, borderTopWidth:0.2, borderTopColor:'grey'}}
+        barStyle={{
+          backgroundColor: Colors.background,
+          width: "100%",
+          elevation: "none",
+          color: Colors.background,
+          borderTopWidth: 0.2,
+          borderTopColor: "grey",
+        }}
         style={styles.bottomBar}
         theme={{
-          "colors": {
-            "secondaryContainer": "transparent",
-            "onSecondaryContainer":Colors.main,
-            "onSurface": Colors.main,
-            "onSurfaceVariant": "grey",
+          colors: {
+            secondaryContainer: "transparent",
+            onSecondaryContainer: Colors.main,
+            onSurface: Colors.main,
+            onSurfaceVariant: "grey",
           },
-          "fonts": {
-            "labelMedium":{ 
-              "fontFamily":'Manrope-Bold', 
-              "fontSize":11
-            }
-          }}}
-        sceneAnimationType='opacity'
-        />
+          fonts: {
+            labelMedium: {
+              fontFamily: "Manrope-Bold",
+              fontSize: 11,
+            },
+          },
+        }}
+        sceneAnimationType="opacity"
+      />
     </PaperProvider>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  topBar:{
-    display:'flex',
-    flexDirection:'row',
-    backgroundColor:Colors.background,
-    justifyContent:'space-between',
-    
+  topBar: {
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: Colors.background,
+    justifyContent: "space-between",
   },
-  bottomBar:{
-
+  bottomBar: {},
+  appbarTitle: {
+    color: Colors.main,
+    fontFamily: "Manrope-ExtraBold",
+    fontSize: 18,
   },
-  appbarTitle:{
-    color:Colors.main,
-    fontFamily:'Manrope-ExtraBold',
-    fontSize:18
+  surface: {
+    width: "100%",
   },
-    surface: {
-    width: '100%',
-  },
-})
+});
