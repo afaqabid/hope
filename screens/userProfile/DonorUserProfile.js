66c626e0f5c7b22765746fcae9cc2cc56df02584
Afaq Abid <afaqabid@gmail.com>
2023-04-03 00:56:23 +0500

    Created Donor Profile Screen.

---
+++ screens/userProfile/DonorUserProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const DonorUserProfile = () => {
+  return (
+    <View>
+      <Text>DonorUserProfile</Text>
+    </View>
+  );
+};
+
+export default DonorUserProfile;
+
+const styles = StyleSheet.create({});
