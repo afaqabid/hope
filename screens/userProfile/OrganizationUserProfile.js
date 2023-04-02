2a07e62702aba0f6ab60ad3bc846e9ea8eada0c2
Afaq Abid <afaqabid@gmail.com>
2023-04-03 00:57:50 +0500

    Updated Navigation in Organization Portal.

---
+++ screens/userProfile/OrganizationUserProfile.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const OrganizationUserProfile = () => {
+  return (
+    <View>
+      <Text>OrganizationUserProfile</Text>
+    </View>
+  );
+};
+
+export default OrganizationUserProfile;
+
+const styles = StyleSheet.create({});
