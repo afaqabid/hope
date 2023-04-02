6edc0956e2911291c9cb55847afc11aba7aac853
Afaq Abid <afaqabid@gmail.com>
2023-04-02 21:24:33 +0500

    Integrated Backend for Donation Post with Firebase.

--- screens/donations/donor/NewPhysicalItemDonationPost.js
+++ screens/donations/donor/NewPhysicalItemDonationPost.js
@@ -11,13 +11,22 @@ import {
   TouchableWithoutFeedback,
 } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Provider as PaperProvider, Text, Button } from "react-native-paper";
+import {
+  Provider as PaperProvider,
+  Text,
+  Button,
+  SegmentedButtons,
+  Banner,
+} from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useFonts } from "expo-font";
 import { useNavigation } from "@react-navigation/native";
 import { auth, db, storage } from "../../../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import * as ImagePicker from "expo-image-picker";
+import Colors from "../../../assets/constants/Colors";
+import * as Location from "expo-location";
+import { ref as dbRef, set } from "firebase/database";
 
 export default function NewPhysicalItemDonationPost() {
   let [fontLoaded] = useFonts({
@@ -30,6 +39,8 @@ export default function NewPhysicalItemDonationPost() {
     "Manrope-SemiBold": require("../../../assets/fonts/Manrope-SemiBold.ttf"),
   });
 
+  const [segBtnValue, setSegBtnValue] = useState("");
+
   const HideKeyboard = ({ children }) => (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       {children}
@@ -38,22 +49,110 @@ export default function NewPhysicalItemDonationPost() {
 
   const navigation = useNavigation();
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let postLoc = "Waiting..";
+  let longitude = 0;
+  let latitude = 0;
+  if (errorMsg) {
+    postLoc = errorMsg;
+  } else if (location) {
+    postLoc = `Latitude: ${location.coords.latitude}, Longitude: ${location.coords.longitude}`;
+    longitude = location.coords.longitude;
+    latitude = location.coords.latitude;
+  }
+
   const [image, setImage] = useState(null);
+  const [imgUrl, setImgUrl] = useState(null);
 
-  // useEffect(  async()=>{
-  //   if(Platform.OS !==web){
-  //     const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  const uploadImage = async () => {
+    // convert image into blob image
+    const blobImage = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function () {
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", image, true);
+      xhr.send(null);
+    });
+    // set metadata of image
+    // Create the file metadata
+    /** @type {any} */
+    const metadata = {
+      contentType: "image/jpeg",
+    };
+    // upload image on storage
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, "DonationPostImages/" + Date.now());
+    const uploadTask = uploadBytesResumable(storageRef, blobImage, metadata);
 
-  //     if(status !== 'granted'){
-  //       alert("Permission Denied!");
-  //     }
-  //   }
-  // }, [])
+    // Listen for state changes, errors, and completion of the upload.
+    await uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
 
-const pickImage = async () => {
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+          setImgUrl(downloadURL);
+        });
+      }
+    );
+  };
+
+  const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
@@ -64,137 +163,111 @@ const pickImage = async () => {
     }
   };
 
-    const uploadImage = async () => {
-      // convert image into blob image
-      const blobImage = await new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-          resolve(xhr.response);
-        };
-        xhr.onerror = function () {
-          reject(new TypeError("Network request failed"));
-        };
-        xhr.responseType = "blob";
-        xhr.open("GET", image, true);
-        xhr.send(null);
-      });
-      // set metadata of image
-      // Create the file metadata
-      /** @type {any} */
-      const metadata = {
-        contentType: "image/jpeg",
-      };
-      // upload image on storage
-      // Upload file and metadata to the object 'images/mountains.jpg'
-      const storageRef = ref(storage, "DonationPostImages/" + Date.now());
-      const uploadTask = uploadBytesResumable(storageRef, blobImage, metadata);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-      // Listen for state changes, errors, and completion of the upload.
-      uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-          const progress =
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log("Upload is " + progress + "% done");
-          switch (snapshot.state) {
-            case "paused":
-              console.log("Upload is paused");
-              break;
-            case "running":
-              console.log("Upload is running");
-              break;
-          }
-        },
-        (error) => {
-          // A full list of error codes is available at
-          // https://firebase.google.com/docs/storage/web/handle-errors
-          switch (error.code) {
-            case "storage/unauthorized":
-              // User doesn't have permission to access the object
-              break;
-            case "storage/canceled":
-              // User canceled the upload
-              break;
-
-            // ...
-
-            case "storage/unknown": 
-              // Unknown error occurred, inspect error.serverResponse
-              break;
-          }
-        },
-        () => {
-          // Upload completed successfully, now we can get the download URL
-          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-            console.log("File available at", downloadURL);
-          });
-        }
-      );
-    };
-
-
-  const uploadPost = () => {
-    if (image != null) {
-      uploadImage();
-      // setImage(null);
+  async function uploadPost() {
+    if (imgUrl == null) {
+      await uploadImage();
     }
-  };
+    if (imgUrl != null) {
+      set(dbRef(db, "hope/donations/" + auth.currentUser.displayName), {
+        imgUrl: imgUrl,
+        title: title,
+        username: auth.currentUser.displayName,
+        category: segBtnValue,
+        description: description,
+        longitude: longitude,
+        latitude: latitude,
+      })
+        .then()
+        .catch((error) => {
+          alert(error);
+        });
+      alert("Post Uploaded Successfully!");
+      navigation.navigate("DonorPortal");
+    } else {
+      alert("Wait! Image is uploading!");
+    }
+  }
 
   return (
     <PaperProvider>
-      <HideKeyboard>
-        <SafeAreaView style={styles.mainContainer}>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === "ios" ? "position" : "height"}
-            style={{ flex: 1 }}
-          >
-            <ScrollView>
-              <View style={styles.imageContainer}>
-                <Text style={styles.titleTxt}>Images</Text>
-                {image && (
-                  <Image
-                    source={{ uri: image }}
-                    style={{ width: "100%", height: "100%" }}
-                  />
-                )}
-              </View>
-              {/* <Button style={styles.btnCancel}>
+      <SafeAreaView style={styles.mainContainer}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "position" : "height"}
+          style={{ flex: 1 }}
+        >
+          <ScrollView>
+            <View style={styles.imageContainer}>
+              <Text style={styles.titleTxt}>Images</Text>
+              {image && (
+                <Image
+                  source={{ uri: image }}
+                  style={{ width: "100%", height: "100%" }}
+                />
+              )}
+            </View>
+            {/* <Button style={styles.btnCancel}>
               <Text style={styles.btnTxtCancel} onPress={Pickimage}>Upload Image</Text>
             </Button> */}
-              <ButtonNative title="Upload Image" onPress={pickImage} />
-              <View style={styles.detailsContainer}>
-                <Text style={styles.titleTxt}>Donation Title</Text>
-                <TextInput
-                  style={styles.donationTitle}
-                  placeholder="Write Donation Title Here."
-                ></TextInput>
-                <Text style={styles.titleTxt}>Description</Text>
-                <TextInput
-                  multiline={true}
-                  style={styles.description}
-                  placeholder="Write Donation Description Here."
-                ></TextInput>
-                <View style={styles.btnContainer}>
-                  <Button style={styles.btnPost} onPress={uploadPost}>
-                    <Text style={styles.btnTxtPost}>Post</Text>
-                  </Button>
-                  <Button style={styles.btnCancel}>
-                    <Text
-                      style={styles.btnTxtCancel}
-                      onPress={() => {
-                        navigation.navigate("NewDonationPost");
-                      }}
-                    >
-                      Cancel
-                    </Text>
-                  </Button>
-                </View>
+            <ButtonNative title="Upload Image" onPress={pickImage} />
+            <View style={styles.detailsContainer}>
+              <Text style={styles.titleTxt}>Title</Text>
+              <TextInput
+                value={title}
+                onChangeText={(title) => setTitle(title)}
+                style={styles.donationTitle}
+                placeholder="Write Donation Title Here."
+              ></TextInput>
+              <Text style={styles.titleTxt}>Select Category</Text>
+              <SegmentedButtons
+                value={segBtnValue}
+                onValueChange={setSegBtnValue}
+                style={{
+                  width: 350,
+                  height: 35,
+                  marginTop: 3,
+                }}
+                buttons={[
+                  {
+                    value: "food",
+                    label: "Food",
+                  },
+                  {
+                    value: "clothes",
+                    label: "Clothes",
+                  },
+                  { value: "goods", label: "Goods" },
+                ]}
+              />
+              <Text style={styles.titleTxt}>Description</Text>
+              <TextInput
+                value={description}
+                multiline={true}
+                style={styles.description}
+                placeholder="Write Donation Description Here."
+                onChangeText={(text) => setDescription(text)}
+              ></TextInput>
+              <View style={styles.btnContainer}>
+                <Button style={styles.btnPost} onPress={uploadPost}>
+                  <Text style={styles.btnTxtPost}>Post</Text>
+                </Button>
+                <Button style={styles.btnCancel}>
+                  <Text
+                    style={styles.btnTxtCancel}
+                    onPress={() => {
+                      navigation.navigate("NewDonationPost");
+                    }}
+                  >
+                    Cancel
+                  </Text>
+                </Button>
               </View>
-            </ScrollView>
-          </KeyboardAvoidingView>
-        </SafeAreaView>
-      </HideKeyboard>
+            </View>
+          </ScrollView>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
     </PaperProvider>
   );
 }
@@ -250,7 +323,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     backgroundColor: "white",
     padding: 10,
-    height: 200,
+    height: 150,
     borderRadius: 5,
   },
 
