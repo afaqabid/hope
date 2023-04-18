611ef947a863364bc89e5992a70c2187e089345c
Afaq Abid <afaqabid@gmail.com>
2023-04-17 22:32:18 -0700

    Testing Github Again.

--- screens/authentication/DonorLogin.js
+++ screens/authentication/DonorLogin.js
@@ -65,7 +65,7 @@ export default function DonorLogin() {
         console.error(error);
       });
   }
-// Testing Github
+// Testing Github Again
   const handleLogin = () => {
     if (email.trim() == "" || password.trim() == "") {
       alert("Please Enter All Fields!");
