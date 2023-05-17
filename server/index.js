4cb17bd84c7604d001525dbd2849a4797de43d4c
afaqabid <afaqabid@gmail.com>
2023-05-18 03:44:58 +0500

    Updated Stripe-Server index.js

--- server/index.js
+++ server/index.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express();
 const port = 19000;
-const PUBLISHABLE_KEY = "pk_test_51MYieUJyFWW0XhyN6ZLddGK5LcjcVneltxGF3WyqTQMKOXUaFJX8TYmX1GPWv8cahXhhkKYHUbF3XOnOuVnT9wpP00Z0QWaasG";
-const SECRET_KEY = "sk_test_51MYieUJyFWW0XhyNjxWnG6seZ4I3oggNko8skNxZSyXbs5mwBvlcvZnEVO9Dxg4PJUiRB6WllIlGnGAhqs1ymPBo00OAu83ZsK";
+const PUBLISHABLE_KEY =
+  "pk_test_51MYieUJyFWW0XhyN6ZLddGK5LcjcVneltxGF3WyqTQMKOXUaFJX8TYmX1GPWv8cahXhhkKYHUbF3XOnOuVnT9wpP00Z0QWaasG";
+const SECRET_KEY =
+  "sk_test_51MYieUJyFWW0XhyNjxWnG6seZ4I3oggNko8skNxZSyXbs5mwBvlcvZnEVO9Dxg4PJUiRB6WllIlGnGAhqs1ymPBo00OAu83ZsK";
 import Stripe from "stripe";
 
 const stripe = Stripe(SECRET_KEY, { apiVersion: "2022-11-15" });
