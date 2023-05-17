549de82975647a5f03468ad9c5267a71ed626fe1
afaqabid <afaqabid@gmail.com>
2023-05-18 03:42:27 +0500

    Updated Chat Tab.

--- screens/chat/ChatTab.js
+++ screens/chat/ChatTab.js
@@ -3,11 +3,12 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Provider as PaperProvider } from 'react-native-paper'
 import ChatHead from '../../components/ChatHead'
+import Colors from '../../assets/constants/Colors'
 
 export default function ChatTab() {
   return (
     <PaperProvider>
-        <View style={{backgroundColor:'#FDFAF6'}} >
+        <View style={{backgroundColor:Colors.white}} >
           <ScrollView>
             <ChatHead></ChatHead>
           </ScrollView>
